1fbd7b2235d7fa3d38cb52f6ab3968565d2e694d
Matthew Sherman <msherman0801@gmail.com>
2019-06-02 17:14:40 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function favoriteIceCream(flavor) {
+    return `I love ${flavor}`
+}
+
+function theTruth() {
+    return true;
+}
+
+function sayHey() {
+    return 'Hey!'
+}
+
+function sayHeyFriend(friend) {
+    return `Hey, ${friend}!`
+}
+
+
+function shouting(shout) {
+    return shout.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
